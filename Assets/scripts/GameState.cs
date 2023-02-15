558679a707093400f0eb5e24ce46cb09fcad81d7
yilinLucywang <yilinwa2@andrew.cmu.edu>
2023-02-15 15:18:05 -0500

    Modified anchors and change the control flow

--- Assets/scripts/GameState.cs
+++ Assets/scripts/GameState.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameState : MonoBehaviour
 {
+    public bool is_p1_turn = true;
+    public int dice_1 = -1; 
+    public int dice_2 = -1;
+
+    public Text num_1_text;
+    public Text num_2_text;  
+
+
+
+    private int cur_step = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,42 @@ public class GameState : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void NextRound()
+    {
+        dice_1 = -1; 
+        dice_2 = -1; 
+        if(is_p1_turn){
+            is_p1_turn = false;
+        }
+        else{
+            is_p1_turn = true;
+        }
+
+        num_1_text.text = "Dice1: "; 
+        num_2_text.text = "Dice2: ";
+    }
+
+    public void RollDice()
+    {
+        int num_1 = Random.Range(1, 5);
+        int num_2 = Random.Range(1, 5);
+        num_1_text.text = "Dice1: " + num_1.ToString(); 
+        num_2_text.text = "Dice2: " + num_2.ToString();
+        dice_1 = num_1; 
+        dice_2 = num_2;
+    }
+
+    public void TopClick(){
+        if(dice_1 != -1){
+            cur_step = dice_1;
+        }
+    }
+
+    public void BottomClick(){
+        if(dice_2 != -1){
+            cur_step = dice_2;
+        }
     }
 }
