29effac79f952f71f66f3bd3cfa27627e637488a
yichenglu-ylu7 <125488002+yichenglu-ylu7@users.noreply.github.com>
2023-03-13 18:08:33 -0400

    add new test camera

---
+++ Assets/scripts/RotateCamera.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float _speed;
+    public GameObject focusPoint;
+    public float constraintVolume;
+
+    private Vector3 _offset;
+    private Vector3 positionForCamera;
+
+    private void Start()
+    {
+        _offset = transform.position - focusPoint.transform.position;
+        positionForCamera = transform.position;
+        
+        constraintVolume = Mathf.Abs(constraintVolume);
+    }
+
+    private void Update()
+    {
+        if (positionForCamera.z < constraintVolume)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                positionForCamera = new Vector3(transform.position.x,transform.position.y, transform.position.z + _speed * Time.deltaTime);
+            }
+        }
+        if(positionForCamera.z > -1*constraintVolume)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                positionForCamera = new Vector3(transform.position.x, transform.position.y, transform.position.z + _speed * -1 * Time.deltaTime);
+            }
+        }
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = positionForCamera;
+        transform.rotation = Quaternion.LookRotation(focusPoint.transform.position - transform.position, Vector3.up);
+    }
+}
