cb363e7849b8634c94b80f23648a31fda9186bc7
yichenglu-ylu7 <125488002+yichenglu-ylu7@users.noreply.github.com>
2023-04-03 16:04:24 -0400

    add highlight and selection
    shader of highlight and selection

---
+++ Assets/scripts/Selection.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Selection : MonoBehaviour
+{
+    public Material highlightMaterial;
+    public Material selectionMaterial;
+
+    private Material originalMaterialHighlight;
+    private Material originalMaterialSelection;
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    void Update()
+    {
+        // Highlight
+        if (highlight != null)
+        {
+            highlight.GetComponent<MeshRenderer>().sharedMaterial = originalMaterialHighlight;
+            highlight = null;
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+        {
+            highlight = raycastHit.transform;
+            if (highlight.CompareTag("Normal") && highlight != selection)
+            {
+                if (highlight.GetComponent<MeshRenderer>().material != highlightMaterial)
+                {
+                    originalMaterialHighlight = highlight.GetComponent<MeshRenderer>().material;
+                    highlight.GetComponent<MeshRenderer>().material = highlightMaterial;
+                }
+            }
+            else if (highlight.CompareTag("Owl") && highlight != selection)
+            {
+                if (highlight.GetComponent<MeshRenderer>().material != highlightMaterial)
+                {
+                    originalMaterialHighlight = highlight.GetComponent<MeshRenderer>().material;
+                    highlight.GetComponent<MeshRenderer>().material = highlightMaterial;
+                }
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
+
+        // Selection
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            if (highlight)
+            {
+                if (selection != null)
+                {
+                    selection.GetComponent<MeshRenderer>().material = originalMaterialSelection;
+                }
+                selection = raycastHit.transform;
+                if (selection.GetComponent<MeshRenderer>().material != selectionMaterial)
+                {
+                    originalMaterialSelection = originalMaterialHighlight;
+                    selection.GetComponent<MeshRenderer>().material = selectionMaterial;
+                }
+                highlight = null;
+            }
+            else
+            {
+                if (selection)
+                {
+                    selection.GetComponent<MeshRenderer>().material = originalMaterialSelection;
+                    selection = null;
+                }
+            }
+        }
+
+    }
+
+}
