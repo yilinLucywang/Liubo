0d44439632d5f85aa131785a110e0a7c071b2dea
lbelford23 <lbelford@andrew.cmu.edu>
2023-04-10 18:01:49 -0400

    Creates basic mouse hover script

---
+++ Assets/scripts/ButtonHover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.LogWarning("Hover Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.LogWarning("Hover Exit");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
