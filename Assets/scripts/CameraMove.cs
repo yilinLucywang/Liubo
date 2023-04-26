5c2096d90ce8e4e083be9209022ee8a961af57fc
yichenglu-ylu7 <125488002+yichenglu-ylu7@users.noreply.github.com>
2023-04-26 17:55:10 -0400

    revert camera angle

--- Assets/scripts/CameraMove.cs
+++ Assets/scripts/CameraMove.cs
@@ -19,53 +19,28 @@ public class CameraMove : MonoBehaviour
     private void Update()
     {
         _offset = transform.position - focusPoint.transform.position;
-        
-        //if ((GameState.state & State.MoveOrPieceSelection) != 0)
-        //{
-            if (Input.GetKey(KeyCode.E)) //|| Input.GetKey(KeyCode.RightArrow))
+
+        if ((GameState.state & State.MoveOrPieceSelection) != 0)
+        {
+            if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.RightArrow))
             {
                 transform.position = focusPoint.transform.position + Quaternion.Euler(0, _speed * -1 * Time.deltaTime, 0) * _offset;
-            } else if (Input.GetKey(KeyCode.Q)) //|| Input.GetKey(KeyCode.LeftArrow))
+            }
+            else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
             {
                 transform.position = focusPoint.transform.position + Quaternion.Euler(0, _speed * Time.deltaTime, 0) * _offset;
             }
-            //Just for recording, plz delete
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-            transform.position = transform.position + new Vector3(0, 1 * Time.deltaTime, 0);
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-            transform.position = transform.position + new Vector3(0, -1 * Time.deltaTime, 0);
+            
         }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-            transform.position = new Vector3(transform.position.x + -1 * Time.deltaTime, transform.position.y , transform.position.z);
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-            transform.position = new Vector3(transform.position.x + 1 * Time.deltaTime, transform.position.y, transform.position.z);
-            }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = transform.position + new Vector3(0, 0, -1 * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = transform.position + new Vector3(0, 0, 1 * Time.deltaTime);
-        }
-        //Until this line
-        //}
 
-        //if((GameState.state & State.MoveOrPieceSelection) == 0)
-        //{
-        //transform.position = initPos;
-        //}
+        if((GameState.state & State.MoveOrPieceSelection) == 0)
+        {
+            transform.position = initPos;
+        }
     }
 
     private void LateUpdate()
     {
-        if(Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Q))
-            transform.rotation = Quaternion.LookRotation(focusPoint.transform.position - transform.position,Vector3.up);
+          transform.rotation = Quaternion.LookRotation(focusPoint.transform.position - transform.position,Vector3.up);
     }
 }
