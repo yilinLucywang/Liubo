30d3fdee70c2f68e66e5a0e5edabec1f84cbf759
yilinLucywang <yilinwa2@andrew.cmu.edu>
2023-04-28 14:44:48 -0400

    Update WriteToGameObject.cs

--- Assets/scripts/WriteToGameObject.cs
+++ Assets/scripts/WriteToGameObject.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using System;
 using System.IO;
 using System.Text;
+using UnityEngine.UI;
+using TMPro;
 
 public class WriteToGameObject : MonoBehaviour
 {
     public List<GameObject> sectionList;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        string path = @"C:\Users\yilinwa2\Documents\GitHub\Liubo_2021\Assets\scripts\tutorialText.csv";
+        string path = @"Assets\scripts\tutorialText.csv";
         string[] lines = System.IO.File.ReadAllLines(path);
         int rowCount = 0;
         foreach(string line in lines)
@@ -19,8 +21,14 @@ public class WriteToGameObject : MonoBehaviour
             string[] columns = line.Split(',');
             int colCount = 0;
             foreach (string column in columns) {
-                //sectionList[colCount].transform.GetChild(rowCount).text = column;
-                Debug.Log(column);
+                if(colCount-12 >= 0 && colCount-12 < 13){
+                    if(rowCount == 0){ 
+                        //sectionList[colCount - 12].transform.GetChild(rowCount).GetComponent<TMP_Text>().text = column;
+                    }
+                    else{
+                        //sectionList[colCount - 12].transform.GetChild(1).GetChild(rowCount - 1).GetComponent<TMP_Text>().text = column;
+                    }
+                }
                 colCount += 1;
             }
             rowCount += 1;
