f238293f5e5774e6be7dcf95dfc3f4ffa36242b7
yilinLucywang <yilinwa2@andrew.cmu.edu>
2023-05-03 11:12:54 -0400

    Update WriteToGameObject.cs

--- Assets/scripts/WriteToGameObject.cs
+++ Assets/scripts/WriteToGameObject.cs
@@ -23,17 +23,27 @@ public class WriteToGameObject : MonoBehaviour
             text += "\n";
         }
         string[] columns = text.Split("$%");
-        Debug.Log(columns[23]);
-        Debug.Log(columns[24]);
+        Debug.Log("check child here");
+        Debug.Log(columns[23]); 
+        Debug.Log(columns[46]); 
+        Debug.Log(columns[69]);
+        Debug.Log(columns[23*4 + 12]);
         for(int i = 0; i < columns.Length; i++){
             int rowNumber = (int)Mathf.Floor(i/24);
-            int colNumber = i%24;
-            if(colNumber > 11 && colNumber < 24){
-                if(rowNumber == 0){ 
-                    sectionList[colNumber - 12].transform.GetChild(0).GetComponent<TMP_Text>().text = columns[i];
-                }
-                else{
-                    sectionList[colNumber - 12].transform.GetChild(1).GetChild(rowNumber-1).GetComponent<TMP_Text>().text = columns[i];
+            int colNumber = i%24 + rowNumber;
+            if(columns[i] == ""){
+                    continue;
+            }
+            else{
+                if(colNumber > 11 && colNumber < 24){
+                    if(rowNumber == 0){ 
+                        sectionList[colNumber - 12].transform.GetChild(0).GetComponent<TMP_Text>().text = columns[i];
+                    }
+                    else{
+                        //Debug.Log(column);
+                        Debug.Log(colNumber.ToString() +"," + rowNumber.ToString() + columns[i]);
+                        sectionList[colNumber - 12].transform.GetChild(1).GetChild(rowNumber-1).GetComponent<TMP_Text>().text = columns[i];
+                    }
                 }
             }
         }
