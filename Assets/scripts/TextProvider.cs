eebfce0c5becaa45ff6547357a8e547182eb0a6e
Chia-Wei Liu <chiawei.liu.1201@gmail.com>
2023-05-03 11:45:33 -0400

    Make textProvider no errors

--- Assets/scripts/TextProvider.cs
+++ Assets/scripts/TextProvider.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TextProvider
 {
+    public static TextProvider Instance;
     // Start is called before the first frame update
     public static TextProvider GetInstance()
     {
@@ -12,5 +13,15 @@ public class TextProvider
             Instance = new TextProvider();
         }
         return Instance;
+    }
+    
+    public TextProvider()
+    {
+        
+    }
+
+    public string GetText(string id)
+    {
+        return "";
     }
 }
